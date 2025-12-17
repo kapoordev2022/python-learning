b1792a2b6e88d50004141baf1c4427508fd7e2dc
kapoordev2022 <kapoor.dev2022@gmail.com>
2025-12-17 20:23:45 +0530

    Feat: Project setup

---
+++ monthly_challenges/challenges/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def january(request):
+    return HttpResponse("Hello, World!")
+
+def february(request):
+    return HttpResponse("Hello, February!")
