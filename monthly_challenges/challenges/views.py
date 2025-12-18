82b1917f7e3dc1f4e4145b228c17f0f9d0071e70
kapoordev2022 <kapoor.dev2022@gmail.com>
2025-12-18 12:37:14 +0530

    Feat: Dynamic URLs

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
-# Create your views here.
-def january(request):
-    return HttpResponse("Hello, World!")
+monthly_challenges = {
+    "january": "Hello, January!",
+    "february": "Hello, February!",
+    "march": "Hello, March!",
+    "april": "Hello, April!",
+    "may": "Hello, May!",
+    "june": "Hello, June!",
+    "july": "Hello, July!",
+    "august": "Hello, August!",
+    "september": "Hello, September!",
+    "october": "Hello, October!",
+    "november": "Hello, November!",
+    "december": "Hello, December!",
+}
 
-def february(request):
-    return HttpResponse("Hello, February!")
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
+
+def monthly_challenge_by_number(request, month):
+    try:
+        challenge_text = list(monthly_challenges.keys())
+        if month > len(challenge_text):
+            return HttpResponseNotFound("This month is not supported!")
+        forword_month = challenge_text[month]
+        return HttpResponseRedirect("/challenges/" + forword_month)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
